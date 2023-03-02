7ae5d265a2c903371ae8decf2e3cee49caa81779
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-01 23:11:38 -0700

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
+      
       <Container header="Example header" direction="horizontal">
         <div>This is an example!</div>
         <div>This is an example!</div>
@@ -13,6 +14,7 @@ function App() {
         <div>This is an example!</div>
         <div>This is an example!</div>
       </Container>
+
       <Container header="Example header" textPosition="center">
         <img
           alt="turtle"
